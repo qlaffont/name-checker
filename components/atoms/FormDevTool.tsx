1ed77317a08d4f728150d92da32cec47ba258269
Quentin <qlaffont13@gmail.com>
2024-03-11 09:28:08 +0100

    fix: fix build

--- components/atoms/FormDevTool.tsx
+++ components/atoms/FormDevTool.tsx
@@ -1,10 +1,10 @@
 import { isDevelopmentEnv } from 'env-vars-validator';
 import dynamic from 'next/dynamic';
-import { useSsr } from 'usehooks-ts';
+import { useIsClient } from 'usehooks-ts';
 
 const DevTool = dynamic(() => import('@hookform/devtools').then((mod) => mod.DevTool), { ssr: false });
 
 export const FormDevTools = ({ control }) => {
-  const { isBrowser } = useSsr();
+  const isBrowser = useIsClient();
   return <>{isDevelopmentEnv() && isBrowser && <DevTool control={control} />}</>;
 };
